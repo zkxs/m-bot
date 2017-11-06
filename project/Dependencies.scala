8c529489b17d2c233e41a4666af08e70fa2612f5
Michael Ripley <zkxs00@gmail.com>
2017-11-05 21:27:56 -0600

    Update dependency to reflect m-bot-module-api rename

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "net.dv8tion" % "JDA" % "3.3.1_295",
     "org.slf4j" % "slf4j-api" % "1.7.25",
     "org.slf4j" % "slf4j-jdk14" % "1.7.25",
-    "net.michaelripley" %% "m-bot-core" % "0.1.0-SNAPSHOT",
+    "net.michaelripley" %% "m-bot-module-api" % "0.1.0-SNAPSHOT",
     "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % "2.9.2",
     "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.2",
     "com.fasterxml.jackson.core" % "jackson-annotations" % "2.9.2",
